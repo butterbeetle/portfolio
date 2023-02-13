e90d3a1a2b29983787373f545a1b11521ea44a5b
butterbeetle <butterbeetle@naver.com>
2023-02-14 00:46:48 +0900

    README 파일 내용 삭제, index.html 필요없는 것 삭제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <>
+      <div>hi</div>
+      <div>hi</div>
+    </>
+  );
 }
 
 export default App;
